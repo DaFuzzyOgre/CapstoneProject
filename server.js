42bb473da040d393ee41f5400f948839019cc842
Kyle Burdette <krburdet@iu.edu>
2022-02-12 12:21:18 -0500

    committing new page: confirmationcancelation

--- server.js
+++ server.js
@@ -28,6 +28,9 @@ app.get("/confirmation", function(req,res){
 app.get("/cancelation", function(req,res){
   res.sendFile(path.join(__dirname,"/views/cancelation.html"));
 });
+app.get("/confirmcancelation", function(req,res){
+  res.sendFile(path.join(__dirname,"/views/confirmcancelation.html"));
+});
 
 // setup http server to listen on HTTP_PORT
 app.listen(HTTP_PORT, onHttpStart);
